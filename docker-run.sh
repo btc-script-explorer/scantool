702605182c4ee23fec8357cbcb68d4770f7ababe
Brian <137178429+nonfungible-human@users.noreply.github.com>
2023-09-15 11:45:42 -0600

    Still making changes to some of the docker scripts.

--- docker-run.sh
+++ docker-run.sh
@@ -5,45 +5,30 @@
 # - Docker Engine installed
 # - use of a config file to start scantool
 
+# Make sure the included a config file and that it exists.
 CONFIG_FILE=$1
 if [ $# -lt 1 ]; then
 	echo "Config file required."
 	exit
 fi
+
 if [ ! -f $CONFIG_FILE ]; then
 	echo "$CONFIG_FILE does not exist."
 	exit
 fi
 
-VERSION=`cat ./VERSION`
-CONTAINER_VERSION=`echo "$VERSION" | sed "s/\./_/g"`
-CONTAINER_NAME="scantool_$CONTAINER_VERSION"
-
-RUNNING_CONTAINER=`echo \`docker container ls\` | awk "/$CONTAINER_NAME/"`
-if [ ${#RUNNING_CONTAINER} -ne 0 ]; then
-	docker stop $CONTAINER_NAME > /dev/null
-fi
-
-#docker image rm scantool:$VERSION > /dev/null
-#docker image rm scantool:latest > /dev/null
-
-EXISTING_CONTAINER=`echo \`docker container ls -a\` | awk "/$CONTAINER_NAME/"`
-if [ ${#EXISTING_CONTAINER} -eq 0 ]; then
-	DOCKER_IMAGE_FILE="scantool-$VERSION-docker-image.tar"
-	docker load -i $DOCKER_IMAGE_FILE > /dev/null
-fi
-
-docker volume create scantool-config-dir > /dev/null
-
+# Check to make sure the user can create a volume and copy files into its data directory.
 DOCKER_DIR="/var/lib/docker"
 DOCKER_VOLUMES_DIR="$DOCKER_DIR/volumes"
 VOLUME_DIR="$DOCKER_VOLUMES_DIR/scantool-config-dir"
 VOLUME_DATA_DIR="$VOLUME_DIR/_data/"
 if [ ! -d $VOLUME_DATA_DIR ]; then
 
+	docker volume create scantool-config-dir > /dev/null
+
 	if [ ! -d $DOCKER_DIR ]; then
 		echo ""
-		echo "$DOCKER_DIR does not exist. Is Docker Engine installed properly? Are you running a recent version?"
+		echo "$DOCKER_DIR does not exist. Is Docker Engine installed properly? Are you running a recent version of docker?"
 		echo ""
 		docker version
 		exit
@@ -52,7 +37,7 @@ if [ ! -d $VOLUME_DATA_DIR ]; then
 	SYSTEM_DOCKER_GROUP=`awk "/docker/" /etc/group`
 	if [ ${#SYSTEM_DOCKER_GROUP} -eq 0 ]; then
 		echo ""
-		echo "Group docker does not exist. Is Docker Engine installed properly? Are you running a recent version?"
+		echo "Group docker does not exist. Is Docker Engine installed properly? Are you running a recent version of docker?"
 		echo ""
 		docker version
 		exit
@@ -62,8 +47,8 @@ if [ ! -d $VOLUME_DATA_DIR ]; then
 	DOCKER_GROUP=`stat -c "%G" $DOCKER_DIR`
 
 	echo ""
-	echo "Access to $VOLUME_DATA_DIR is required."
-	echo "Run the following commands."
+	echo "Write access to $VOLUME_DATA_DIR is required."
+	echo "Run the following commands, then run this script again."
 	echo ""
 	if [ "$DOCKER_GROUP" != "docker" ]; then
 		echo "sudo chown $DOCKER_OWNER:docker $DOCKER_DIR"
@@ -80,12 +65,6 @@ if [ ! -d $VOLUME_DATA_DIR ]; then
 	IN_DOCKER=`echo \`groups\` | awk "/docker/"`
 	if [ ${#IN_DOCKER} -eq 0 ]; then
 		echo "sudo usermod -a -G docker $USER"
-	fi
-
-	echo "./docker-run.sh $1"
-
-	if [ ${#IN_DOCKER} -eq 0 ]; then
-		echo ""
 		echo "You will probably need to log out and log in again before the group membership takes effect."
 	fi
 
@@ -95,14 +74,32 @@ fi
 
 cp $CONFIG_FILE $VOLUME_DATA_DIR
 
-#echo "To stop scantool run:"
-#echo "docker stop scantool"
+# Create a container name.
+VERSION=`cat ./VERSION`
+CONTAINER_VERSION=`echo "$VERSION" | sed "s/\./_/g"`
+CONTAINER_NAME="scantool_$CONTAINER_VERSION"
 
-#echo "Web site is at http://$IP_ADDRESS/web/"
-#echo "REST API is at http://$IP_ADDRESS/rest/v1/"
+# Check to make sure the container is not already running.
+# If it is running, inform the user.
+RUNNING_CONTAINER=`echo \`docker container ls\` | awk "/$CONTAINER_NAME/"`
+if [ ${#RUNNING_CONTAINER} -ne 0 ]; then
+#	docker stop $CONTAINER_NAME > /dev/null
+	echo "Container $CONTAINER_NAME is already running."
+	echo "Run \"docker stop $CONTAINER_NAME\" to stop it, then run this script again."
+	exit
+fi
 
-docker run --name $CONTAINER_NAME -p 80:80 -v scantool-config-dir:/scantool-config-dir scantool
+# If the container exists, start it up.
+EXISTING_CONTAINER=`echo \`docker container ls -a\` | awk "/$CONTAINER_NAME/"`
+if [ ${#EXISTING_CONTAINER} -ne 0 ]; then
+	docker container start -i $CONTAINER_NAME
+	exit
+fi
 
-#IP_ADDRESS=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' scantool:$VERSION`
+# Load the images, create a new container and run it.
+docker load -i scantool-$VERSION-docker-image.tar > /dev/null
+docker run --name $CONTAINER_NAME -p 80:80 -v scantool-config-dir:/scantool-config-dir scantool:$VERSION
+
+#docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' scantool:$VERSION
 #docker inspect -f '{{.Id}}' scantool
 
